695f0184da2bb5997ace387aed872d6348e96bf2
Adam <sector-f@users.noreply.github.com>
2017-08-09 11:45:33 -0400

    improved mkdungeon.sh

--- mkdungeon.sh
+++ mkdungeon.sh
@@ -3,6 +3,8 @@
 mkdir -p dungeons || exit 1
 cd dungeons || exit 2
 
+cargo build --release
+
 while IFS= read -r seed; do
-	cargo run --release "$seed"
+	../target/release/mount_arelle "$seed"
 done < <(tr -dc a-zA-Z0-9 < /dev/urandom | fold -w 32 | head -n 50)
