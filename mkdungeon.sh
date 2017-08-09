47585ae3fc6289115a52b47659c3d3221b4dd76e
Adam <sector-f@users.noreply.github.com>
2017-08-09 11:25:46 -0400

    Made mkdungeon.sh call cargo run

--- mkdungeon.sh
+++ mkdungeon.sh
@@ -4,5 +4,5 @@ mkdir -p dungeons || exit 1
 cd dungeons || exit 2
 
 while IFS= read -r seed; do
-	../target/release/mount_arelle "$seed"
+	cargo run --release "$seed"
 done < <(tr -dc a-zA-Z0-9 < /dev/urandom | fold -w 32 | head -n 50)
